654e42673e09c1761d919fa27fefde5e478ace42
Adolfo Sánchez Lopéz <81250601+Adolfsan99@users.noreply.github.com>
2024-07-03 20:07:26 -0500

    Cambios de encriptación

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ function encriptar() {
     entrada = entrada.replace(/i/g, "3");
     entrada = entrada.replace(/o/g, "4");
     entrada = entrada.replace(/u/g, "5");
-    entrada = entrada.replace(/A/g, "6");
-    entrada = entrada.replace(/E/g, "7");
-    entrada = entrada.replace(/I/g, "8");
-    entrada = entrada.replace(/O/g, "9");
-    entrada = entrada.replace(/U/g, "0");
+    //entrada = entrada.replace(/A/g, "6");
+    //entrada = entrada.replace(/E/g, "7");
+    //entrada = entrada.replace(/I/g, "8");
+    //entrada = entrada.replace(/O/g, "9");
+    //entrada = entrada.replace(/U/g, "0");
 
     // Reemplazos con los valores encriptados
     entrada = entrada.replace(/1/g, "ai");
@@ -36,11 +36,11 @@ function encriptar() {
     entrada = entrada.replace(/3/g, "imes");
     entrada = entrada.replace(/4/g, "ober");
     entrada = entrada.replace(/5/g, "ufat");
-    entrada = entrada.replace(/6/g, "AI");
-    entrada = entrada.replace(/7/g, "ENTER");
-    entrada = entrada.replace(/8/g, "IMES");
-    entrada = entrada.replace(/9/g, "OBER");
-    entrada = entrada.replace(/0/g, "UFAF");
+    //entrada = entrada.replace(/6/g, "AI");
+    //entrada = entrada.replace(/7/g, "ENTER");
+    //entrada = entrada.replace(/8/g, "IMES");
+    //entrada = entrada.replace(/9/g, "OBER");
+    //entrada = entrada.replace(/0/g, "UFAF");
 
     actualizarSalida(entrada, true);
   }
@@ -55,12 +55,12 @@ function desencriptar() {
   entrada = entrada.replace(/ober/g, "o");
   entrada = entrada.replace(/ufat/g, "u");
   entrada = entrada.replace(/mes/g, "");
-  entrada = entrada.replace(/AI/g, "A");
-  entrada = entrada.replace(/ENTER/g, "E");
-  entrada = entrada.replace(/IMES/g, "I");
-  entrada = entrada.replace(/OBER/g, "O");
-  entrada = entrada.replace(/UFAF/g, "U");
-  entrada = entrada.replace(/MES/g, "");
+  //entrada = entrada.replace(/AI/g, "A");
+  //entrada = entrada.replace(/ENTER/g, "E");
+  //entrada = entrada.replace(/IMES/g, "I");
+  //entrada = entrada.replace(/OBER/g, "O");
+  //entrada = entrada.replace(/UFAF/g, "U");
+  //entrada = entrada.replace(/MES/g, "");
 
   document.getElementById("entrada-usuario").value = entrada;
   actualizarSalida(entrada, false);
